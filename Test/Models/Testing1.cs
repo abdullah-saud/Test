2c504983a245630ddb25723725e909ed3ba66359
animeshranjan <47320518+animeshranjan@users.noreply.github.com>
2019-02-21 15:12:40 +0530

    initial commit

---
+++ Test/Models/Testing1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class Testing1
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+
+    }
+}
